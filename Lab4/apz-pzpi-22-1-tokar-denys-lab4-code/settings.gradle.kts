4ea9fa98b8c42e52c7bcc785a7a375600db20bac
NureTokarDenys <denys.tokar@nure.ua>
2025-06-11 17:13:17 +0300

    lab 4 mobile setup

---
+++ Lab4/apz-pzpi-22-1-tokar-denys-lab4-code/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "apz-pzpi-22-1-tokar-denys-lab4-code"
+include(":app")
+ 
